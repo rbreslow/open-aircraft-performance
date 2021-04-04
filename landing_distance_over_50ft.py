f690402849ab9ca47d4ad94320ce825e6354fbff
Rocky Breslow <rbreslow@azavea.com>
2021-04-03 22:19:50 -0400

    Use a quadratic fit to model palt / oat relationship

--- landing_distance_over_50ft.py
+++ landing_distance_over_50ft.py
@@ -3,7 +3,14 @@ import math
 def step_one(palt, oat):
     # TODO: We're off by around 15-20ft compared to ForeFlight's calculation at
     # KPNE. Who is more accurate? Does it matter?
-    return (0.00012178 * palt + 2.85562) * oat + (0.034075 * palt + 1672.9)
+    # Linear Fit
+    # return (0.00012178 * palt + 2.85562) * oat + (0.034075 * palt + 1672.9)
+
+    # Quadratic Fit
+    a = -5E-11 * pow(palt, 2) + 2E-07 * palt + 0.0032
+    b = 4E-09 * pow(palt, 2) + 9E-05 * palt + 2.8827
+    c = 9E-07 * pow(palt, 2) + 0.0274 * palt + 1677.4
+    return  a * pow(oat, 2) + b * oat + c
 
 def step_two(res, weight):
     # TODO: Explain why we think this ratio makes sense. Also, Leijurv is based.
